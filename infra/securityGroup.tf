faec2ea2b6f76d5fbc9428c41d54bdd1bbd7aa38
matheusrodacki <matheus.rodacki@outlook.com>
2023-03-01 20:01:42 -0300

    edit roles and add new security group

--- infra/securityGroup.tf
+++ infra/securityGroup.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "app_lb" {
   vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_security_group_rule" "allow_tcp" {
+resource "aws_security_group_rule" "inbound_lb" {
   type              = "ingress"
   from_port         = 8000
   to_port           = 8000
@@ -13,11 +13,35 @@ resource "aws_security_group_rule" "allow_tcp" {
   security_group_id = aws_security_group.app_lb.id
 }
 
-resource "aws_security_group_rule" "allow_tcp" {
+resource "aws_security_group_rule" "outbound_lb" {
   type              = "egress"
   from_port         = 0
-  to_port           = 65535
+  to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.app_lb.id
 }
+
+resource "aws_security_group" "private_group" {
+  name        = "private_ecs"
+  description = "Allow private traffic for ECS Loud Balancer"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "inbound_private_traffic" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  source_security_group_id = aws_security_group.app_lb.id
+  security_group_id = aws_security_group.private_group.id
+}
+
+resource "aws_security_group_rule" "outbound_private_traffic" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  source_security_group_id = aws_security_group.app_lb.id
+  security_group_id = aws_security_group.private_group.id
+  }
