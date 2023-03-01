4c331f72366231ded8fb4935dd7521fd20c920e7
matheusrodacki <matheus.rodacki@outlook.com>
2023-03-01 18:55:35 -0300

    remove unsused variables

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,8 +1,5 @@
 module "production" {
   source = "../../infra"
   repoName = "prod_repo"
-  description = "production-application"
-  max = 5
-  instance = "t2.micro"
-  env = "production-environment"
+  
 }
