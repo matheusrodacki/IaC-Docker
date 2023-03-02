bad7183f5c7443bf47857ab88b6f2c3aa5cfdc47
matheusrodacki <matheus.rodacki@outlook.com>
2023-03-02 20:46:48 -0300

    add aim.tf

---
+++ infra/iam.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_role" "cargo" {
+  name = "${var.cargoIAM}"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = ["ec2.amazonaws.com",
+                     "ecs-tasks.amazonaws.com"]
+        }
+      },
+    ]
+  })
+
+  
+}
+
+resource "aws_iam_role_policy" "ecs_ecr" {
+  name = "ecs_ecr"
+  role = aws_iam_role.cargo.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          ,
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
