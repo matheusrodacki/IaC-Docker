d34b650ae1cdf9463a6ef92303b765b29e33de07
matheusrodacki <matheus.rodacki@outlook.com>
2023-03-10 19:25:18 -0300

    add k8s.tf

---
+++ infra/kubernetes.tf
@@ -0,0 +1,55 @@
+resource "kubernetes_deployment" "Django-API" {
+  metadata {
+    name = "django-api"
+    labels = {
+      nome = "django"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        nome = "django"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          nome = "django"
+        }
+      }
+
+      spec {
+        container {
+          image = "221201751884.dkr.ecr.us-west-2.amazonaws.com/production:v1"
+          name  = "django"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/clientes/"
+              port = 8000
+
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
+  }
+}
--- infra/provider.tf
+++ infra/provider.tf
@@ -12,3 +12,18 @@ terraform {
 provider "aws" {
   region  = "us-west-2"
 }
+data "aws_eks_cluster" "default" {
+  name = var.cluster_name
+  depends_on = [ module.eks.eks_managed_node_groups ]
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+  
+}
