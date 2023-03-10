3cc7b373e8ea6c6ca975fc2cd6685f05aa64d148
matheusrodacki <matheus.rodacki@outlook.com>
2023-03-10 18:21:30 -0300

    edit security group for support EKS

--- infra/securityGroup.tf
+++ infra/securityGroup.tf
@@ -1,46 +1,22 @@
-resource "aws_security_group" "alb" {
-  name        = "alb"
+resource "aws_security_group" "ssh_cluster" {
+  name        = "ssh_cluster"
   vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_security_group_rule" "tcp_alb" {
+resource "aws_security_group_rule" "ssh_cluster_in" {
   type              = "ingress"
-  from_port         = 8000
-  to_port           = 8000
+  from_port         = 22
+  to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.ssh_cluster.id
 }
 
-resource "aws_security_group_rule" "out_alb" {
+resource "aws_security_group_rule" "ssh_cluster_out" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.ssh_cluster.id
 }
-
-resource "aws_security_group" "private" {
-  name        = "private_ecs"
-  description = "Allow private traffic for ECS Loud Balancer"
-  vpc_id      = module.vpc.vpc_id
-}
-
-resource "aws_security_group_rule" "inbound_ecs" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  source_security_group_id = aws_security_group.alb.id
-  security_group_id = aws_security_group.private.id
-}
-
-resource "aws_security_group_rule" "outbound_ecs" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.private.id
-  }
