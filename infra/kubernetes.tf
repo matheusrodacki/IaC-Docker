1233ed1cf5df7629dc3c66ebbf2712590e15a502
matheusrodacki <matheus.rodacki@outlook.com>
2023-03-18 09:53:41 -0300

    dont fix

--- infra/kubernetes.tf
+++ infra/kubernetes.tf
@@ -54,21 +54,21 @@ resource "kubernetes_deployment" "Django-API" {
   }
 }
 
-resource "kubernetes_service" "LoadBalancer" {
-  metadata {
-    name = "loadbalancer-django-api"
-  }
-  spec {
-    selector = {
-      nome = "django"
-    }
-    port {
-      port        = 8000
-      target_port = 8000
-    }
-    type = "LoadBalancer"
-  }
-}
+#resource "kubernetes_service" "LoadBalancer" {
+#  metadata {
+#    name = "loadbalancer-django-api"
+#  }
+#  spec {
+#    selector = {
+#      nome = "django"
+#    }
+#    port {
+#      port        = 8000
+#      target_port = 8000
+#    }
+#    type = "LoadBalancer"
+#  }
+#}
 
 #data "kubernetes_service" "DNSpath" {
 #    metadata {
--- infra/provider.tf
+++ infra/provider.tf
@@ -12,14 +12,15 @@ terraform {
 provider "aws" {
   region  = "us-west-2"
 }
-data "aws_eks_cluster" "default" {
-  name = var.cluster_name
-  depends_on = [ module.eks.eks_managed_node_groups ]
-}
 
-data "aws_eks_cluster_auth" "default" {
-  name = var.cluster_name
-}
+#data "aws_eks_cluster" "default" {
+#  name = var.cluster_name
+#  depends_on = [ module.eks.eks_managed_node_groups ]
+#}
+#
+#data "aws_eks_cluster_auth" "default" {
+#  name = var.cluster_name
+#}
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
